72bc77c45c64b7dbeeb62143c1e8e8419aeff223
Patryk <patryk.galazka@euvic.pl>
2019-04-24 23:02:51 +0200

    event mapper implemented

--- eventscreen/src/main/java/pl/branchdev/eventscreen/domain/mapper/EventDtoToEventMapper.kt
+++ eventscreen/src/main/java/pl/branchdev/eventscreen/domain/mapper/EventDtoToEventMapper.kt
@@ -1,2 +1,15 @@
 package pl.branchdev.eventscreen.domain.mapper
 
+import pl.branchdev.data.EventDto
+import pl.branchdev.eventscreen.model.Event
+
+class EventDtoToEventMapper {
+    fun mapToEvent(eventDto: EventDto): Event {
+        return Event(
+            title = eventDto.title,
+            subtitle = eventDto.subtitle,
+            thumbnailUrl = eventDto.imageUrl,
+            dateInformation = eventDto.date
+        )
+    }
+}
--- eventscreen/src/main/java/pl/branchdev/eventscreen/model/Event.kt
+++ eventscreen/src/main/java/pl/branchdev/eventscreen/model/Event.kt
@@ -1,2 +1,3 @@
 package pl.branchdev.eventscreen.model
 
+data class Event(val title:String, val subtitle:String, val thumbnailUrl:String, val dateInformation: String)
